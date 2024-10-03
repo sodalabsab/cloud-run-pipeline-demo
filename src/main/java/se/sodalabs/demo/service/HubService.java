da716e6372c44aecd64a747d69452bf285a7da14
Daniel Fröding <daniel.froding@gmail.com>
2024-10-03 10:08:38 +0200

    Removed some unused code

--- src/main/java/se/sodalabs/demo/service/HubService.java
+++ src/main/java/se/sodalabs/demo/service/HubService.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+//import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -89,6 +89,7 @@ public class HubService {
         String.class);
   }
 
+  /* 
   private static ResponseEntity<String> returnParsedHubResponse(
       ResponseEntity<String> responseFromHub) {
     if (responseFromHub.getStatusCode().is2xxSuccessful()) {
@@ -98,7 +99,7 @@ public class HubService {
     } else {
       return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
-  }
+  }*/
 
   public String getLastRegisteredAt() {
     return lastRegisteredAt;
